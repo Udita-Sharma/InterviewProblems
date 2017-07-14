fda9258f33ccd05d25f7ab840f1ce2b473e7a3bc
Udita Vinod Sharma <usharma@manh.com>
2017-07-14 20:58:30 +0530

    1.3

--- DiamondProblem/DiamondProblem.cpp
+++ DiamondProblem/DiamondProblem.cpp
@@ -6,73 +6,40 @@ using namespace std;
 class A
 {
 	public:
-	A(){ cout << "A's default constructor" << endl; }
-	A(int x){cout<<"A's constructor"<<endl;}
+	A(){ cout << "A's  constructor" << endl; }
 	~A(){cout<<"A's destructor"<<endl;}
-	void function1()
-	{
-		cout << "A's function1()" << endl;
-	}
+	
 };
 class B:public A
 {
 	public:
-	B(int x):A(x)
+	B()
 	{cout<<"B's constructor"<<endl;}
 	~B(){cout<<"B's destructor"<<endl;}
-	void function1()
-	{
-		cout << "B's function1()"<<endl;
-	}
+	
 };
 class C:public A
 {
 	public:
-	C(int x):A(x)
+	C()
 	{cout<<"C's constructor"<<endl;}
 	~C(){cout<<"C's destructor"<<endl;}
-	void function1()
-	{
-		cout << "C's function1()" << endl;
-	}
+	
 };
 class D: public B, public C
 {
 	public:
-	D(int x):B(x),C(x)
+	D()
 	{cout<<"D's constructor"<<endl;}
-	void function1()
-	{
-		cout << "D's function1()" << endl;
-	}
 	~D(){cout<<"D's destructor"<<endl;}
 };
 	
 int main()
 {
-	//D d(10);
-	A *b = new B(10);
-	b->function1();
-	delete b;
-	/*A's constructor
-	  B's constructor
-	  A's function1()
-	  A's destructor*/
-	
-	
-	//A *d = new D(10); //COmpile time error
-	//d->function1();
-	//delete d;
-
-	/*DiamondProblem.cpp:61 : 17 : error : ‘A’ is an ambiguous base of ‘D’
-		A *d = new D(10);*/
-	
-
-
-
+	D d;
 	return 0;
 }
-/*output:
+/*
 A's constructor
 B's constructor
 A's constructor
@@ -83,5 +50,4 @@ C's destructor
 A's destructor
 B's destructor
 A's destructor
-
 */
--- DiamondProblem/SolutionThroughVirtual.cpp
+++ DiamondProblem/SolutionThroughVirtual.cpp
@@ -5,72 +5,54 @@ using namespace std;
 class A
 {
 public:
-	A(){ cout << "A's default constructor" << endl; }
-	A(int x){ cout << "A's constructor" << endl; }
-	virtual ~A(){ cout << "A's destructor" << endl; }
-	virtual void function1()
-	{
-		cout << "A's function1()";
-	}
+	A(){ cout << "A's constructor" << endl; }
+	~A(){ cout << "A's destructor" << endl; }
+	
 };
 class B :virtual public A
 {
 public:
-	B(int x) :A(x)
+	B()
 	{
 		cout << "B's constructor" << endl;
 	}
-	virtual ~B(){ cout << "B's destructor" << endl; }
-	void function1()
-	{
-		cout << "B's function1()" << endl;
-	}
+	~B(){ cout << "B's destructor" << endl; }
+	
 };
 class C :virtual public A
 {
 public:
-	C(int x) :A(x)
+	C()
 	{
 		cout << "C's constructor" << endl;
 	}
-	virtual ~C(){ cout << "C's destructor" << endl; }
-	void function1()
-	{
-		cout << "C's function1()";
-	}
+	~C(){ cout << "C's destructor" << endl; }
+	
 };
 class D : public B, public C
 {
 public:
-	D(int x) :B(x), C(x)
+	D()
 	{
 		cout << "D's constructor" << endl;
 	}
-	void function1()
-	{
-		cout << "D's function1()" << endl;
-	}
+	
 	~D(){ cout << "D's destructor" << endl; }
 };
 
 int main()
 {
-	//D d(10);
-	A *d = new D(10);
-	d->function1();
-	delete d;
+	D d;
 	return 0;
 }
 /*
-In the above program, constructor of ‘A’ is called once. One important thing to note in the above output is, the default constructor of ‘A’ is called. When we use ‘virtual’ keyword,
-the default constructor of grandparent class is called by default even if the parent classes explicitly call parameterized constructor.
-o/p:
-A's default constructor
+In the above program, constructor of ‘A’ is called once. 
+A's constructor
 B's constructor
 C's constructor
 D's constructor
-D's function1()
 D's destructor
 C's destructor
 B's destructor
 A's destructor
+*/
